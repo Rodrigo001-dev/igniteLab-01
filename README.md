e0b955ea91b6f5d72feee35e06c45989e7acbe2a
Rodrigo <rodrigorael53@gmail.com>
2023-01-29 02:49:10 -0300

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Funcionalidades
+
+### Serviço de compras(purchases)
+
+- [Admin] Cadastro de produtos
+- [Admin] Listagem de produtos
+
+- [Auth] Listagem de compras
+
+- [Public] Compra de um produto
+- [Public] Lista de produtos disponíveis p/ compra
+
+### Serviço de sala de aula (classroom)
+
+- [Admin] Listar matrículas
+- [Admin] Listar alunos
+- [Admin] Listar cursos
+- [Admin] Cadastrar cursos
+
+- [Auth] Listar cursos que tenho acesso
+- [Auth] Acessar conteúdo do curso
---
+++ purchases/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { HttpModule } from './http/http.module';
+
+// o Module é um arquivo que basicamente reúne um parte da nossa aplicação
+// ele une vários outros arquivos em uma coisa só, ou seja, o module agrupa
+// vários controllers e providers(providers é todo arquivo que não for controller)
+// a regra de negócio sempre vai estar dentro de um arquivo de services
+@Module({
+  imports: [DatabaseModule, HttpModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
