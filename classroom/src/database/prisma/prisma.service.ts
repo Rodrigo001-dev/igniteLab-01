7c7710930603f06c93ae3de775bd2f45c81c8795
Rodrigo <rodrigorael53@gmail.com>
2023-01-29 20:40:49 -0300

    :sparkles: feat: backend setting

---
+++ classroom/src/database/prisma/prisma.service.ts
@@ -0,0 +1,33 @@
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  INestApplication,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    // o super chama o constructor da classe que o PrismaService estende, ou seja
+    // vai chamar o constructor do PrismaClient
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
--- purchases/src/http/auth/authorization.guard.ts
+++ purchases/src/http/auth/authorization.guard.ts
@@ -5,6 +5,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import jwt from 'express-jwt';
 import { expressJwtSecret } from 'jwks-rsa';
 import { promisify } from 'node:util';
@@ -26,9 +27,11 @@ export class AuthorizationGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const httpContext = context.switchToHttp();
-    const req = httpContext.getRequest();
-    const res = httpContext.getResponse();
+    // const httpContext = context.switchToHttp();
+    // const req = httpContext.getRequest();
+    // const res = httpContext.getResponse();
+
+    const { req, res } = GqlExecutionContext.create(context).getContext();
 
     const checkJWT = promisify(
       jwt({
