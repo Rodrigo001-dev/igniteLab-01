a9bdef432b4e612348a2b155bd174cfab9323058
Rodrigo <rodrigorael53@gmail.com>
2023-01-30 02:05:05 -0300

    :sparkles: feat: create product routes

---
+++ purchases/src/http/graphql/models/product.ts
@@ -0,0 +1,13 @@
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+
+@ObjectType()
+export class Product {
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  title: string;
+
+  @Field()
+  slug: string;
+}
